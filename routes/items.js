b12b0babbfba932d6caf29c48829616648c5e8f1
Shashank Kumar <shsharma93@gmail.com>
2019-09-26 17:58:13 +1000

    updated

--- routes/items.js
+++ routes/items.js
@@ -23,7 +23,7 @@ router.post('/', auth, async (req, res) => {
   }
 
   try {
-    const allItems = [];
+    const updatedItems = [];
     for (let i in req.body.items) {
       const item = await Items.findOne(
         _.pick(req.body.items[i], ['type', 'color', 'size'])
@@ -32,14 +32,14 @@ router.post('/', auth, async (req, res) => {
       if (item) {
         item.stock += req.body.items[i].stock;
         await item.save();
-        allItems.push(item._id);
+        updatedItems.push(item._id);
       } else {
         const newItem = new Items({ ...req.body.items[i] });
         await newItem.save();
-        allItems.push(newItem._id);
+        updatedItems.push(newItem._id);
       }
     }
-    res.json({ success: true, itemIds: allItems });
+    res.json({ success: true, itemIds: updatedItems });
   } catch (error) {
     return res
       .status(400)
